54f7c8a97f16db01ce697a3291272110983dc106
Konstantin Krukhmalev <Konstantin.Krukhmalev@jetbrains.com>
2021-10-17 23:57:32 +0300

    Add method to stop ProductsServer

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/Main.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/Main.java
@@ -7,12 +7,21 @@ import ru.akirakozov.sd.refactoring.server.ProductsServer;
  * 
  * Main class for starting application
  * Usage: java <app-path> [port [dbConnectionString]] 
+ * 
+ * Defaults:
+ * * port = 8081
+ * * dbConnectionString = "jdbc:sqlite:test.db"
  */
 public class Main {
     public static void main(String[] args) throws Exception {
         int port = args.length >= 1 ? Integer.parseInt(args[0]) : 8081;
         String databaseConnectionString = args.length >= 2 ? args[1] : "jdbc:sqlite:test.db";
         
-        new ProductsServer(port, databaseConnectionString).start();
+        ProductsServer server = new ProductsServer(port, databaseConnectionString);
+        try {
+            server.start();
+        } finally {
+            server.stop();
+        }
     }
 }
