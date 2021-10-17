37e8e88367aaff78c51e710a43f40d358867a69f
Konstantin Krukhmalev <Konstantin.Krukhmalev@jetbrains.com>
2021-10-17 19:32:47 +0300

    Optimize imports

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
@@ -1,11 +1,9 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
-import ru.akirakozov.sd.refactoring.database.ProductsDatabaseManager;
 import ru.akirakozov.sd.refactoring.html.ProductsHtmlBuilder;
 import ru.akirakozov.sd.refactoring.products.Product;
 import ru.akirakozov.sd.refactoring.products.ProductsManager;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
